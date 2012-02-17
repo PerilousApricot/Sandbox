7f7d44b2fcfbc2e48f24453a87925749be345336
Andrew Melo <andrew.melo@gmail.com>
2012-02-17 14:02:32 -0600

    nose.run returns the wrong thing, oopsw

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@
 
 import nose, sys
 
-sys.exit( nose.run( argv=[__file__, '--with-xunit', '-v','tests','-m', '(_t.py$)|(_t$)|(^test)'] ) )
+if nose.run( argv=[__file__, '--with-xunit', '-v','tests','-m', '(_t.py$)|(_t$)|(^test)'] ):
+    sys.exit(0)
+else:
+    sys.exit(1)
 
